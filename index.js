b4d50742388c129b2b16ffdd4c7cbaadb5de0374
lemonsandlimes <bobarctor1001@gmail.com>
2019-09-07 13:16:31 -0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,17 +11,14 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
   socket.on('disconnect', function(){
     console.log('a user disconnected');
   });
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-  });
-  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
-  socket.on('nickname', function(msg){
-    io.emit('nickname', msg);
   });
 });
 
